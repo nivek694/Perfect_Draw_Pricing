d4430b50baf548e236b29b068f027b22ae3227c9
Kevin Hayes <khhayes2@uncg.edu>
2026-04-27 19:32:25 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files (HTML, JS) from the current directory
+app.use(express.static('./public'));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
